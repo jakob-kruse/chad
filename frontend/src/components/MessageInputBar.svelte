b2ea9d18e67710d7e1521f34bdf5be5c02a0af44
Jakob Kruse <j.kruse@shopware.com>
2020-06-21 15:00:49 +0200

    Fixed Message Input being visible on name input

--- frontend/src/components/MessageInputBar.svelte
+++ frontend/src/components/MessageInputBar.svelte
@@ -1,7 +1,7 @@
 <script>
   import { sendMessage as socketSendMessage } from "../socket.js";
   import InputField from "./InputField.svelte";
-
+  import { isInitialized } from "../stores";
   let inputValue = "";
 
   function sendMessage() {
@@ -13,17 +13,19 @@
   }
 </script>
 
-<div
-  class="fixed flex w-full p-2 bottom-0 border-solid border-t-2 border-gray-300
-  bg-white z-50">
-  <InputField
-    placeholder="Type a message..."
-    bind:value={inputValue}
-    on:enter={sendMessage} />
-  <button
-    class="text-blue-500 border-solid border-2 border-blue-500 hover:text-white
-    hover:bg-blue-500 font-bold py-1 px-2 ml-2 rounded"
-    on:click={sendMessage}>
-    Send
-  </button>
-</div>
+{#if $isInitialized}
+  <div
+    class="fixed flex w-full p-2 bottom-0 border-solid border-t-2
+    border-gray-300 bg-white z-50">
+    <InputField
+      placeholder="Type a message..."
+      bind:value={inputValue}
+      on:enter={sendMessage} />
+    <button
+      class="text-blue-500 border-solid border-2 border-blue-500
+      hover:text-white hover:bg-blue-500 font-bold py-1 px-2 ml-2 rounded"
+      on:click={sendMessage}>
+      Send
+    </button>
+  </div>
+{/if}
